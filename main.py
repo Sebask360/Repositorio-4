37a2a5a726391db1700bdd5986f2497688f778a5
Sebask360 <sebastiancusism@gmail.com>
2024-07-15 23:27:55 -0500

    agregue el str y llevo el conteo del numero de intentos

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import random 
 def adivina_el_numero():
-    numero_secreto = random.randint(1, 10)
-    intentos = 0
-    adivinado = False
+    numero_secreto = random.randint(1, 10) #genera el numero secreto aleatoriamente
+    intentos = 0 #Inicializa el contador de intentos
+    adivinado = False #determina si el numero ha sido adivinado
     
-    print("Bienvenido al juego de adivinanza de numeros")
-    print("Estoy pensando en un numero entre 1 y 10")
+    print("Bienvenido al juego de adivinanza de numeros") #Imprime
+    print("Estoy pensando en un numero entre 1 y 10") #Imprime
     
     while not adivinado:
-        intento = int(input("Adivina el numero: "))
-        intentos += 1
+        intento = int(input("Adivina el numero: ")) #Pide que alimine el numero de intentos
+        intentos += 1 #Incrementa el numero de intentos
         
         if intento < numero_secreto:
             print("Demasiado bajo, intenta de nuevo. ")
         elif intento > numero_secreto:
             print("Demasiado alto, intenta de nuevo. ")
         else:
-            adivinado = True
-            print("Felicidades, adivinaste el numero en {intentos} intentos. ")
+            adivinado = True #cambia el estado del numero a encontrado. 
+            print("Felicidades, adivinaste el numero en " + str(intentos) + " intentos.")
 
 adivina_el_numero()
