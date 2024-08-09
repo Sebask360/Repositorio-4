8fb8769d853221e40bd7568ba523c1c36330da48
Sebask360 <sebastiancusism@gmail.com>
2024-08-08 19:29:10 -0500

    Agregue un maximo de intentos, y un contador de intentos.

--- Adivina_el_juego.py
+++ Adivina_el_juego.py
@@ -5,24 +5,30 @@ def guess_the_number():
         secret_number = random.randint(1, 100)
         attemps = 0
         guessed = False
+        max_attemps = 8 
 
         print("¡Bienvenido al juego de adivinanza de números!")
         print("Estoy pensando en un número entre 1 y 100.")
+        print(f"Tienes {max_attemps} intentos para adivinarlo. ")
 
-        while not guessed:
-            try:
-                attemp = int(input("Adivina el número: "))
-                attemps += 1
+        while not guessed and attemps < max_attemps:
+                try:
+                    attemp = int(input("Adivina el número: "))
+                    attemps += 1
 
-                if attemp < secret_number:
-                    print("Demasiado bajo. Intenta de nuevo.")
-                elif attemp > secret_number:
-                    print("Demasiado alto. Intenta de nuevo.")
-                else:
-                    guessed = True
-                    print(f"¡Felicidades! Adivinaste el número en {attemps} intentos.")
-            except ValueError:
-                print("Por favor, ingresa un número válido.")
+                    if attemp < secret_number:
+                        print(f"Demasiado bajo. Intenta de nuevo. Te quedan {max_attemps - attemps} intentos. ")
+                    elif attemp > secret_number:
+                        print(f"Demasiado alto. Intenta de nuevo. Te quedan {max_attemps - attemps} intentos. ")
+                    else:
+                        guessed = True
+                        print(f"¡Felicidades! Adivinaste el número en {attemps} intentos.")
+                except ValueError:
+                    print("Por favor, ingresa un número válido.")
+            
+        if not guessed:
+                print(f"Haz alcanzado el maximo de {max_attemps} intentos: ")
+                print(f"El numero secreto era: {secret_number} ")
 
         # Preguntar al usuario si quiere jugar de nuevo
         play_again = input("¿Quieres jugar de nuevo? (s/n): ").strip().lower()
@@ -32,5 +38,3 @@ def guess_the_number():
 
 # Llamar a la función para iniciar el juego
 guess_the_number()
-
-
